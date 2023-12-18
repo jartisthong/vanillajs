b54683c4bbdfe84125389b8b868aa4d163a312a0
jartisthong <129035599+jartisthong@users.noreply.github.com>
2023-12-18 21:49:26 +0900

    유저에게 인사하기

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const loginForm = document.getElementById("login-form");
 const loginInput = loginForm.querySelector("input");
+const greeting = document.querySelector("#greeting")
+const HIDDEN_CLASSNAME = "hidden";
 
-
-function onLoginSubmit() {
+function onLoginSubmit(event) {
+    event.preventDefault();
     const username = loginInput.value;
-        console.log(username);
+    loginForm.classList.add("hidden");
+    console.log(username);
+    greeting.innerText = "Hello "+ username;
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-
-loginForm.addEventListener("submit", onLoginSubmit)
+loginForm.addEventListener("submit", onLoginSubmit);
