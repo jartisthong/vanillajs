36914ec65a3c7d83a2891b56ba96b1f3ce854f89
jartisthong <129035599+jartisthong@users.noreply.github.com>
2024-01-08 14:26:37 +0900

    삭제할 수 있는 투두리스트

--- js/todo.js
+++ js/todo.js
@@ -2,12 +2,32 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput =toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+function deleteTodo(event) {
+    const li = event.target.parentElement;
+    console.log(`${event.target.parentElement.innerText} is done!`);
+    li.remove();
+
+    // 어디에 속해있는지 거슬러올라가서 확인한다음 그걸 지워줄 것임. 
+}
+
+
+function paintTodo(newTodo) {
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    const button = document.createElement("button");
+    button.innerText = "❀";
+    button.addEventListener("click", deleteTodo);
+    span.innerText = newTodo;
+    li.appendChild(span);
+    li.appendChild(button);
+    toDoList.appendChild(li);
+}
 
 function handleToDoSubmit(event) {
     event.preventDefault();
     const newTodo = toDoInput.value;
-    console.log(toDoInput.value);
     toDoInput.value = "";
+    paintTodo(newTodo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
